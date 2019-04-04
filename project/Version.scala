4107f79094785d969554d707a1342a5ae2dcdb44
AndreyVS <andrey@2lemetry.ru>
2019-04-04 09:36:02 +0300

    Update Version.scala
    update akka version

--- project/Version.scala
+++ project/Version.scala
@@ -2,7 +2,7 @@ object Version {
   val project = "0.9.0"
   val scala = "2.12.4"
   val crossScala = Seq("2.11.11", "2.12.4")
-  val akka = "2.5.9"
+  val akka = "2.5.19"
   val sprayJson = "1.3.3"
   val rediscala = "1.8.0"
   val dispatch = "0.12.0"
