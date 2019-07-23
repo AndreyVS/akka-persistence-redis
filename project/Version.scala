d92d3a026ff8f1d8901969117db7d8adf4ea2465
Andrey Stepanov <andrey@2lemetry.ru>
2019-07-23 10:18:07 +0300

    update deps

--- project/Version.scala
+++ project/Version.scala
@@ -1,8 +1,8 @@
 object Version {
-  val project = "0.9.0"
-  val scala = "2.12.4"
+  val project = "0.9.1"
+  val scala = "2.12.8"
   val crossScala = Seq("2.11.11", "2.12.4")
-  val akka = "2.5.19"
+  val akka = "2.5.23"
   val sprayJson = "1.3.3"
   val rediscala = "1.8.0"
   val dispatch = "0.12.0"
